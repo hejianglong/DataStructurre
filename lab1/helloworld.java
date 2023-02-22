7b01341a56f002e45f7d91367064ce3a1f3765f3
Space_explorer <2358275241@qq.com>
2023-02-22 21:03:51 +0800

    completed first part of lab1

---
+++ lab1/helloworld.java
@@ -0,0 +1,12 @@
+public class helloworld {
+	public static void main(String[] args) {
+		int x = 0;
+		int sum = 0;
+		while(x < 10) {
+			// System.out.print(x + " ");
+			System.out.print(sum + " ");
+			x = x + 1;
+			sum += x;
+		}
+	}
+}
